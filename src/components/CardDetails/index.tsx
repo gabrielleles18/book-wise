e4f6520e6d133d5aa2844f8bac31c6a735055a47
gabrielleles18 <gabriel.leles18@gmail.com>
2024-06-04 14:21:52 -0300

    Componets

---
+++ src/components/CardDetails/index.tsx
@@ -0,0 +1,9 @@
+import {CardDetailsContainer} from "./styles";
+
+export function CardDetails() {
+    return (
+        <CardDetailsContainer>
+            <h1>Card Details</h1>
+        </CardDetailsContainer>
+    );
+}
---
+++ src/components/CardPopular/index.tsx
@@ -0,0 +1,9 @@
+import {CardPopularContainer} from "./styles";
+
+export function CardPopular() {
+    return (
+        <CardPopularContainer>
+            <h1>Card Popular</h1>
+        </CardPopularContainer>
+    );
+}
